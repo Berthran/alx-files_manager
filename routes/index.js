680c9632048d39e7ed87bab7829c0e0d41e29b58
Daniel Berthran <berthlearn@gmail.com>
2024-12-11 16:29:54 +0100

    add /users route to router object

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Import the AppController, which contains the logic for handling the routes
 const AppController = require('../controllers/AppController');
 
+// Import the UsersController, which contains the logic for handling the user routes
+const UsersController = require('../controllers/UsersController')
+
 // Create a new router object to define modular routes
 const router = express.Router();
 
@@ -16,10 +19,17 @@ router.get('/status', AppController.getStatus);
 
 /**
  * Define a GET route for '/stats'.
- * - When a GET request is made to '/stats', the getStats method in AppController is called.
- * - The getStats method will handle sending the response.
+ * When a GET request is made to '/stats', the getStats method in AppController is called.
+ * The getStats method will handle sending the response.
  */
 router.get('/stats', AppController.getStats);
+
+/**
+ * Define a POST route for '/users'.
+ * When a POST request is made to '/users', the postNew method in UsersControler is called.
+ * The postNew methode will handle sending the response
+ */
+router.post('/users', UsersController.postNew);
 
 /**
  * Export the router object so it can be used in other files.

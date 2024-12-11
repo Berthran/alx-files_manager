c6fdcb0133655b2c5107e68005ce3788c468d97e
Daniel Berthran <berthlearn@gmail.com>
2024-12-11 18:14:46 +0100

    add routes to router object for Authentication

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ const express = require('express');
 // Import the AppController, which contains the logic for handling the routes
 const AppController = require('../controllers/AppController');
 
-// Import the UsersController, which contains the logic for handling the user routes
+// Import the UsersController, which contains the logic for handling the user-related routes
 const UsersController = require('../controllers/UsersController');
+
+// Importing the AuthController module, which handles authentication-related routes and logic
+const AuthController = require('../controllers/AuthController');
 
 // Create a new router object to define modular routes
 const router = express.Router();
@@ -30,6 +33,27 @@ router.get('/stats', AppController.getStats);
  * The postNew methode will handle sending the response
  */
 router.post('/users', UsersController.postNew);
+
+/**
+ * Define a GET route for '/connect'.
+ * When a GET request is made to '/connect', the getConnect method in AuthController is called.
+ * The getConnect method will handle the authentication logic, such as user login.
+ */
+router.get('/connect', AuthController.getConnect);
+
+/**
+ * Define a GET route for '/disconnect'.
+ * When a GET request is made to '/disconnect', the getDisconnect method in AuthController is called
+ * The getDisconnect method will handle the user logout or disconnection process.
+ */
+router.get('/disconnect', AuthController.getDisonnect);
+
+/**
+ * Define a GET route for '/users/me'.
+ * When a GET request is made to '/users/me', the getMe method in UserController is called.
+ * The getMe method will handle fetching and returning the data of the currentlyauthenticated user.
+ */
+router.get('/users/me', UsersController.getMe);
 
 /**
  * Export the router object so it can be used in other files.
